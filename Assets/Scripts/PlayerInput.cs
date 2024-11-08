d1c531818576b428b91e7e0bcd7adb8db2dd1ef5
unknown <Troanh1508@gmail.com>
2024-11-08 13:27:18 +0700

    Replaced manual shooting with auto shooting for player

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -9,19 +9,20 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField]
     private Camera mainCamera;
-    public Button shootButton;
+    // public Button shootButton;
 
-    public Joystick joystick;
+    public Joystick moveJoystick;
+    public Joystick aimJoystick;
     public UnityEvent<Vector2> OnMoveBody = new UnityEvent<Vector2>();
     public UnityEvent<Vector2> OnMoveTurret = new UnityEvent<Vector2>();
 
-    void Start(){
-        if (shootButton != null)
-        {
-            // Add a listener to the button
-            shootButton.onClick.AddListener(OnButtonClick);
-        }
-    }
+    // void Start(){
+    //     if (shootButton != null)
+    //     {
+    //         // Add a listener to the button
+    //         shootButton.onClick.AddListener(OnButtonClick);
+    //     }
+    // }
     private void Awake() {
         if (mainCamera == null)
             mainCamera = Camera.main;
@@ -30,32 +31,37 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (joystick != null)
+        if (moveJoystick != null)
         {
             GetBodyMovement();
+        }
+        if (aimJoystick != null)
+        {
             GetTurretMovement();   
         }
+
+        GetComponentInChildren<TankController>().HandleShoot(); //Shoot as soon as ready
     }
 
     
     private void GetTurretMovement()
     {
-        Vector2 movementVector = new Vector2(joystick.Horizontal,joystick.Vertical);
+        Vector2 movementVector = new Vector2(aimJoystick.Horizontal,aimJoystick.Vertical);
         OnMoveTurret?.Invoke(movementVector.normalized);
     }
 
     private void GetBodyMovement()
     {
-        Vector2 movementVector = new Vector2(joystick.Horizontal,joystick.Vertical);
+        Vector2 movementVector = new Vector2(moveJoystick.Horizontal,moveJoystick.Vertical);
         OnMoveBody?.Invoke(movementVector.normalized);
     }
 
 
-    void OnButtonClick()
-    {
-        //Debug.Log("Button clicked!");
-        GetComponentInChildren<TankController>().HandleShoot();
-    }
+    // void OnButtonClick()
+    // {
+    //     //Debug.Log("Button clicked!");
+    //     GetComponentInChildren<TankController>().HandleShoot();
+    // }
 
 
 }
