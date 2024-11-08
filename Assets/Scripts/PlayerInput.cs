b42c0f2404f484ba8b77e186f91dc4f64763290e
unknown <Troanh1508@gmail.com>
2024-11-09 00:24:24 +0700

    working on toogle auto-shoot setting

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -9,20 +9,21 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField]
     private Camera mainCamera;
-    // public Button shootButton;
+    public Button shootButton;
 
     public Joystick moveJoystick;
     public Joystick aimJoystick;
+    public Joystick fixedAimJoystick;
     public UnityEvent<Vector2> OnMoveBody = new UnityEvent<Vector2>();
     public UnityEvent<Vector2> OnMoveTurret = new UnityEvent<Vector2>();
 
-    // void Start(){
-    //     if (shootButton != null)
-    //     {
-    //         // Add a listener to the button
-    //         shootButton.onClick.AddListener(OnButtonClick);
-    //     }
-    // }
+    void Start(){
+        if (shootButton != null)
+        {
+            // Add a listener to the button
+            shootButton.onClick.AddListener(OnButtonClick);
+        }
+    }
     private void Awake() {
         if (mainCamera == null)
             mainCamera = Camera.main;
@@ -39,14 +40,23 @@ public class PlayerInput : MonoBehaviour
         {
             GetTurretMovement();   
         }
+        if (fixedAimJoystick != null)
+        {
+            GetFixedTurretMovement();
+        }
 
-        GetComponentInChildren<TankController>().HandleShoot(); //Shoot as soon as ready
+        // GetComponentInChildren<TankController>().HandleShoot(); //Shoot as soon as ready
     }
 
     
     private void GetTurretMovement()
     {
         Vector2 movementVector = new Vector2(aimJoystick.Horizontal,aimJoystick.Vertical);
+        OnMoveTurret?.Invoke(movementVector.normalized);
+    }
+    private void GetFixedTurretMovement()
+    {
+        Vector2 movementVector = new Vector2(fixedAimJoystick.Horizontal,fixedAimJoystick.Vertical);
         OnMoveTurret?.Invoke(movementVector.normalized);
     }
 
@@ -57,11 +67,11 @@ public class PlayerInput : MonoBehaviour
     }
 
 
-    // void OnButtonClick()
-    // {
-    //     //Debug.Log("Button clicked!");
-    //     GetComponentInChildren<TankController>().HandleShoot();
-    // }
+    void OnButtonClick()
+    {
+        //Debug.Log("Button clicked!");
+        GetComponentInChildren<TankController>().HandleShoot();
+    }
 
 
 }
