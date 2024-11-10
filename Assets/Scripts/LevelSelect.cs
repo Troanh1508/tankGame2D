56b6c177be71954a61660c4fac84ae3303b61c22
unknown <Troanh1508@gmail.com>
2024-11-10 23:59:12 +0700

    fix level texts not displaying number when first playing the game/ no json savedata

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -14,13 +14,15 @@ public class LevelSelect : MonoBehaviour
     private string saveLocation;
     void Start()
     {
+        for (int i = 0; i < levelButtons.Length; i++) {
+            int level = i+1;
+            levelButtons[i].GetComponentInChildren<TextMeshProUGUI>().SetText(level.ToString());
+        }
         saveLocation = Path.Combine(Application.persistentDataPath, "tankGameData.json");
         if (File.Exists(saveLocation))
         {
             SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
-            for (int i = 0; i< levelButtons.Length; i++){
-                int level = i+1;
-                levelButtons[i].GetComponentInChildren<TextMeshProUGUI>().SetText(level.ToString());
+            for (int i = 0; i < levelButtons.Length; i++) {
                 if (i + 1 > saveData.highestSceneIndex) {
                     levelButtons[i].interactable = false;
                 }
